6620b22d18ec3820d3889cd8dfbe77b12461a393
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2022-12-16 04:04:02 -0800

    update html and add metadata to images

--- short_example/proc.js
+++ short_example/proc.js
@@ -18,12 +18,21 @@ function loadData() {
     document.getElementById('x_' + rawData.axes[0].id).click();
     document.getElementById('autoScaleImages').onchange = updateScaling;
     fillTable();
+    startAutoScroll();
+}
+
+function escapeHtml(text) {
+    return text.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
+}
+
+function unescapeHtml(text) {
+    return text.replaceAll('&lt;', '<').replaceAll('&gt;', '>').replaceAll('&quot;', '"').replaceAll('&#039;', "'").replaceAll('&amp;', '&');
 }
 
 function fillTable() {
     var x = document.querySelector('input[name="x_axis_selector"]:checked').id.substring(2);
     var y = document.querySelector('input[name="y_axis_selector"]:checked').id.substring(2);
-    var table = document.getElementById("image_table");
+    var table = document.getElementById('image_table');
     var newContent = "<th>";
     var xAxis = null;
     var yAxis = null;
@@ -36,33 +45,43 @@ function fillTable() {
         }
     }
     for (var val of xAxis.values) {
-        newContent += "<td title=\"" + val.description + "\">" + val.title + "</td>";
+        if (!document.getElementById('showval_' + xAxis.id + '__' + val.key).checked) {
+            continue;
+        }
+        newContent += '<td title="' + val.description.replaceAll('"', "&quot;") + '">' + val.title + '</td>';
     }
     newContent += "</th>";
     for (var val of yAxis.values) {
-        newContent += "<tr><td title=\"" + val.description + "\">" + val.title + "</td>";
+        if (!document.getElementById('showval_' + yAxis.id + '__' + val.key).checked) {
+            continue;
+        }
+        newContent += '<tr><td title="' + val.description.replaceAll('"', '&quot;') + '">' + val.title + '</td>';
         var url = "";
         for (var subAxis of rawData.axes) {
             if (subAxis.id == x) {
-                url += "/{X}"
+                url += '/{X}';
             }
             else if (subAxis.id == y) {
-                url += "/" + val.key;
+                url += '/' + val.key;
             }
             else {
                 var valSelectedKey;
                 for (var subVal of subAxis.values) {
-                    if (window.getComputedStyle(document.getElementById('tab_' + subAxis.id + '__' + subVal.key)).display != "none") {
+                    if (window.getComputedStyle(document.getElementById('tab_' + subAxis.id + '__' + subVal.key)).display != 'none') {
                         valSelectedKey = subVal.key;
                     }
                 }
-                url += "/" + valSelectedKey;
+                url += '/' + valSelectedKey;
             }
         }
         for (var xVal of xAxis.values) {
-            newContent += "<td><img class=\"table_img\" src=\"" + url.replace("{X}", xVal.key).substring(1) + "." + rawData.ext + "\" /></td>";
+            if (!document.getElementById('showval_' + xAxis.id + '__' + xVal.key).checked) {
+                continue;
+            }
+            var actualUrl = url.replace('{X}', xVal.key).substring(1) + '.' + rawData.ext;
+            newContent += '<td><img class="table_img" id="autogen_img_' + escapeHtml(actualUrl).replace(' ', '%20') + '" onclick="doPopupFor(this)" src="' + actualUrl + '" /></td>';
         }
-        newContent += "</tr>";
+        newContent += '</tr>';
         if (x == y) {
             break;
         }
@@ -73,6 +92,7 @@ function fillTable() {
 
 function updateScaling() {
     var percent;
+    var xAxis;
     if (document.getElementById('autoScaleImages').checked) {
         var x = document.querySelector('input[name="x_axis_selector"]:checked').id.substring(2);
         for (var axis of rawData.axes) {
@@ -80,14 +100,208 @@ function updateScaling() {
                 xAxis = axis;
             }
         }
-        percent = (90 / xAxis.values.length) + "vw";
+        percent = (90 / xAxis.values.length) + 'vw';
     }
     else {
         percent = "";
     }
-    for (var image of document.getElementById("image_table").getElementsByClassName("table_img")) {
+    for (var image of document.getElementById('image_table').getElementsByClassName('table_img')) {
         image.style.width = percent;
     }
+}
+
+function toggleDescriptions() {
+    var show = document.getElementById('showDescriptions').checked;
+    for (var cName of ['tabval_subdiv', 'axis_table_cell']) {
+        for (var elem of document.getElementsByClassName(cName)) {
+            if (show) {
+                elem.classList.remove('tab_hidden');
+            }
+            else {
+                elem.classList.add('tab_hidden');
+            }
+        }
+    }
+}
+
+function toggleShowVal(axis, val) {
+    var show = document.getElementById('showval_' + axis + '__' + val).checked;
+    var element = document.getElementById('clicktab_' + axis + '__' + val);
+    if (show) {
+        element.classList.remove('tab_hidden');
+    }
+    else {
+        element.classList.add('tab_hidden');
+    }
+    fillTable();
+}
+
+var anyRangeActive = false;
+
+const timer = ms => new Promise(res => setTimeout(res, ms));
+
+function enableRange(id) {
+    var range = document.getElementById('range_tablist_' + id);
+    var label = document.getElementById('label_range_tablist_' + id);
+    range.oninput = function() {
+        anyRangeActive = true;
+        label.innerText = (range.value/2) + ' seconds';
+    }
+    var data = {};
+    data.range = range;
+    data.counter = 0;
+    data.id = id;
+    data.tabPage = document.getElementById('tablist_' + id);
+    data.tabs = data.tabPage.getElementsByClassName('nav-link');
+    return data;
+}
+
+async function startAutoScroll() {
+    var rangeSet = [];
+    for (var axis of rawData.axes) {
+        rangeSet.push(enableRange(axis.id));
+    }
+    while (true) {
+        await timer(500);
+        if (!anyRangeActive) {
+            continue;
+        }
+        for (var data of rangeSet) {
+            if (data.range.value <= 0) {
+                continue;
+            }
+            data.counter++;
+            if (data.counter < data.range.value) {
+                continue;
+            }
+            data.counter = 0;
+            var next = false;
+            for (var tab of data.tabs) {
+                if (tab.classList.contains('active')) {
+                    next = true;
+                }
+                else if (tab.classList.contains('tab_hidden')) {
+                    // Skip past
+                }
+                else if (next) {
+                    tab.click();
+                    next = false;
+                    break;
+                }
+            }
+            if (next) { // Click the first non-hidden
+                for (var tab of data.tabs) {
+                    if (tab.classList.contains('tab_hidden')) {
+                        // Skip past
+                    }
+                    else {
+                        tab.click();
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+function genParamQuote(text) {
+    // Referenced to match generation_parameters_copypaste.py - quote(text)
+    if (!text.includes(',')) {
+        return text;
+    }
+    return '"' + text.toString().replaceAll('\\', '\\\\').replaceAll('"', '\\"') + '"';
+}
+
+function formatMet(name, val, bad) {
+    if (val == null) {
+        return "";
+    }
+    val = val.toString();
+    if (bad !== undefined && val == bad) {
+        return "";
+    }
+    return name + ": " + genParamQuote(val) + ", "
+}
+
+function formatMetadata(valSet) {
+    var count = Object.keys(valSet).length;
+    if (count == 0) {
+        return "";
+    }
+    else if (count == 1) {
+        return valSet["error"];
+    }
+    // Referenced to match processing.py - create_infotext(p)
+    var negative = valSet["negativeprompt"];
+    if (negative.length > 0) {
+        negative = "\nNegative prompt: " + negative;
+    }
+    // Keys we have but gen-param doesn't: VAE, sigma(churn/tmin/tmax/noise)
+    var keyData = formatMet("Steps", valSet["steps"])
+        + formatMet("Sampler", valSet["sampler"])
+        + formatMet("CFG scale", valSet["cfgscale"])
+        + formatMet("Seed", valSet["seed"])
+        // face restore
+        + formatMet("Size", valSet["width"] + "x" + valSet["height"])
+        // model hash
+        + formatMet("Model", valSet["model"])
+        + formatMet("Hypernet", valSet["hypernetwork"], "none")
+        + formatMet("Hypernet strength", valSet["hypernetworkstrength"], "none")
+        // Batch size, batch pos
+        + formatMet("Variation seed", valSet["varseed"], "0")
+        + formatMet("Variation seed strength", valSet["varstrength"], "0")
+        // Seed resize from
+        + formatMet("Denoising strength", valSet["denoising"])
+        // Conditional mask weight
+        + formatMet("Eta", valSet["eta"])
+        + formatMet("Clip skip", valSet["clipskip"], "1")
+        // ENSD
+        ;
+    keyData = keyData.substring(0, keyData.length - 2);
+    return valSet["prompt"] + negative + "\n" + keyData;
+}
+
+function crunchMetadata(url) {
+    if (!('metadata' in rawData)) {
+        return {};
+    }
+    initialData = rawData.metadata;
+    var index = 0;
+    for (var part of url.substring(0, url.indexOf('.')).split('/')) {
+        var axis = rawData.axes[index++];
+        var actualVal = null;
+        for (var val of axis.values) {
+            if (val.key == part) {
+                actualVal = val;
+                break;
+            }
+        }
+        if (actualVal == null) {
+            return { "error": "metadata parsing failed for part " + index + ": " + part };
+        }
+        for (var [key, value] of Object.entries(actualVal.params)) {
+            if (key == "promptreplace") {
+                var replacers = value.split('=', 2);
+                var match = replacers[0].trim();
+                var replace = replacers[1].trim();
+                initialData["prompt"] = initialData["prompt"].replaceAll(match, replace);
+                initialData["negativeprompt"] = initialData["negativeprompt"].replaceAll(match, replace);
+            }
+            else {
+                initialData[key] = value;
+            }
+        }
+    }
+    return initialData;
+}
+
+function doPopupFor(img) {
+    var modalElem = document.getElementById('image_info_modal');
+    var url = img.id.substring('autogen_img_'.length);
+    var params = escapeHtml(formatMetadata(crunchMetadata(unescapeHtml(url)))).replaceAll("\n", "\n<br>");
+    var text = 'Image: ' + url + (params.length > 1 ? ', parameters: <br>' + params : '<br>(parameters hidden)');
+    modalElem.innerHTML = '<div class="modal-dialog" style="display:none">(click outside image to close)</div><div class="modal_inner_div"><img class="popup_modal_img" src="' + unescapeHtml(url) + '"><br><div class="popup_modal_undertext">' + text + '</div>';
+    $('#image_info_modal').modal('toggle');
 }
 
 loadData();
