af197be868f0f2d0817ac8bc4885df0d70ea6d2c
Alex "mcmonkey" Goodwin <git_commits@alexgoodwin.dev>
2022-12-30 03:52:53 -0800

    update short_example page data per generator update

--- short_example/proc.js
+++ short_example/proc.js
@@ -3,23 +3,104 @@
  */
 
 function loadData() {
+    document.getElementById('x_' + rawData.axes[0].id).click();
+    document.getElementById('x2_none').click();
+    document.getElementById('y2_none').click();
     // rawData.ext/title/description
     for (var axis of rawData.axes) {
         // axis.id/title/description
         for (var val of axis.values) {
             // val.key/title/description
-            document.getElementById('clicktab_' + axis.id + '__' + val.key).onclick = fillTable;
+            document.getElementById('clicktab_' + axis.id + '__' + val.key).addEventListener('click', fillTable);
         }
-        document.getElementById('x_' + axis.id).onclick = fillTable;
-        document.getElementById('y_' + axis.id).onclick = fillTable;
+        for (var prefix of ['x_', 'y_', 'x2_', 'y2_']) {
+            document.getElementById(prefix + axis.id).addEventListener('click', fillTable);
+        }
+        for (var label of ['x2_none', 'y2_none']) {
+            document.getElementById(label).addEventListener('click', fillTable);
+        }
         console.log(document.getElementById('x_' + axis.id))
     }
     console.log("Loaded data for '" + rawData.title + "'");
-    document.getElementById('x_' + rawData.axes[0].id).click();
-    document.getElementById('autoScaleImages').onchange = updateScaling;
+    document.getElementById('autoScaleImages').addEventListener('change', updateScaling);
     fillTable();
     startAutoScroll();
 }
+
+function getAxisById(id) {
+    for (var axis of rawData.axes) {
+        if (axis.id == id) {
+            return axis;
+        }
+    }
+}
+
+function getNextAxis(axes, startId) {
+    var next = false;
+    for (var subAxis of axes) {
+        if (subAxis.id == startId) {
+            next = true;
+        }
+        else if (next) {
+            return subAxis;
+        }
+    }
+    return null;
+}
+
+function getSelectedValKey(axis) {
+    for (var subVal of axis.values) {
+        if (window.getComputedStyle(document.getElementById('tab_' + axis.id + '__' + subVal.key)).display != 'none') {
+            return subVal.key;
+        }
+    }
+    return null;
+}
+
+window.addEventListener('keydown', function(kbevent) {
+    var elem = document.activeElement;
+    if (!elem.id.startsWith('clicktab_')) {
+        return;
+    }
+    var axisId = elem.id.substring('clicktab_'.length);
+    var splitIndex = axisId.indexOf('__');
+    axisId = axisId.substring(0, splitIndex);
+    var axis = getAxisById(axisId);
+    if (kbevent.key == "ArrowLeft") {
+        var tabPage = document.getElementById('tablist_' + axis.id);
+        var tabs = tabPage.getElementsByClassName('nav-link');
+        var newTab = clickTabAfterActiveTab([].slice.call(tabs).reverse());
+        newTab.focus();
+    }
+    else if (kbevent.key == "ArrowRight") {
+        var tabPage = document.getElementById('tablist_' + axis.id);
+        var tabs = tabPage.getElementsByClassName('nav-link');
+        var newTab = clickTabAfterActiveTab(tabs);
+        newTab.focus();
+    }
+    else if (kbevent.key == "ArrowUp") {
+        var next = getNextAxis(rawData.axes.slice().reverse(), axisId);
+        if (next != null) {
+            var selectedKey = getSelectedValKey(next);
+            var swapToTab = this.document.getElementById('clicktab_' + next.id + '__' + selectedKey);
+            swapToTab.focus();
+        }
+    }
+    else if (kbevent.key == "ArrowDown") {
+        var next = getNextAxis(rawData.axes, axisId);
+        if (next != null) {
+            var selectedKey = getSelectedValKey(next);
+            var swapToTab = this.document.getElementById('clicktab_' + next.id + '__' + selectedKey);
+            swapToTab.focus();
+        }
+    }
+    else {
+        return;
+    }
+    kbevent.preventDefault();
+    kbevent.stopPropagation();
+    return false;
+}, true);
 
 function escapeHtml(text) {
     return text.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
@@ -29,78 +110,115 @@ function unescapeHtml(text) {
     return text.replaceAll('&lt;', '<').replaceAll('&gt;', '>').replaceAll('&quot;', '"').replaceAll('&#039;', "'").replaceAll('&amp;', '&');
 }
 
+function canShowVal(axis, val) {
+    return document.getElementById('showval_' + axis + '__' + val).checked;
+}
+
+function getXAxisContent(x, y, xAxis, val, x2Axis, x2val, y2Axis, y2val) {
+    var url = "";
+    for (var subAxis of rawData.axes) {
+        if (subAxis.id == x) {
+            url += '/{X}';
+        }
+        else if (subAxis.id == y) {
+            url += '/' + val.key;
+        }
+        else if (x2Axis != null && subAxis.id == x2Axis.id) {
+            url += '/' + x2val.key;
+        }
+        else if (y2Axis != null && subAxis.id == y2Axis.id) {
+            url += '/' + y2val.key;
+        }
+        else {
+            url += '/' + getSelectedValKey(subAxis);
+        }
+    }
+    var newContent = '';
+    for (var xVal of xAxis.values) {
+        if (!canShowVal(xAxis.id, xVal.key)) {
+            continue;
+        }
+        var actualUrl = url.replace('{X}', xVal.key).substring(1) + '.' + rawData.ext;
+        newContent += '<td><img class="table_img" id="autogen_img_' + escapeHtml(actualUrl).replace(' ', '%20') + '" onclick="doPopupFor(this)" src="' + actualUrl + '" /></td>';
+    }
+    return newContent;
+}
+
+function optDescribe(isFirst, val) {
+    return isFirst && val != null ? '<span title="' + escapeHtml(val.description) + '"><b>' + val.title + '</b></span><br>' : (val != null ? '<br>' : '');
+}
+
 function fillTable() {
-    var x = document.querySelector('input[name="x_axis_selector"]:checked').id.substring(2);
-    var y = document.querySelector('input[name="y_axis_selector"]:checked').id.substring(2);
+    var x = getCurrentSelectedAxis('x');
+    var y = getCurrentSelectedAxis('y');
+    var x2 = getCurrentSelectedAxis('x2');
+    var y2 = getCurrentSelectedAxis('y2');
+    console.log('Do fill table, x=' + x + ', y=' + y + ', x2=' + x2 + ', y2=' + y2);
+    var xAxis = getAxisById(x);
+    var yAxis = getAxisById(y);
+    var x2Axis = x2 == 'None' || x2 == x || x2 == y ? null : getAxisById(x2);
+    var y2Axis = y2 == 'None' || y2 == x2 || y2 == x || y2 == y ? null : getAxisById(y2);
     var table = document.getElementById('image_table');
-    var newContent = "<th>";
-    var xAxis = null;
-    var yAxis = null;
-    for (var axis of rawData.axes) {
-        if (axis.id == x) {
-            xAxis = axis;
-        }
-        if (axis.id == y) {
-            yAxis = axis;
-        }
-    }
-    for (var val of xAxis.values) {
-        if (!document.getElementById('showval_' + xAxis.id + '__' + val.key).checked) {
+    var newContent = '<th>';
+    for (var x2val of (x2Axis == null ? [null] : x2Axis.values)) {
+        if (x2val != null && !canShowVal(x2Axis.id, x2val.key)) {
             continue;
         }
-        newContent += '<td title="' + val.description.replaceAll('"', "&quot;") + '">' + val.title + '</td>';
+        var x2first = true;
+        for (var val of xAxis.values) {
+            if (!canShowVal(xAxis.id, val.key)) {
+                return '';
+            }
+            newContent += '<th title="' + val.description.replaceAll('"', "&quot;") + '">' + optDescribe(x2first, x2val) + val.title + '</th>';
+            x2first = false;
+        }
     }
-    newContent += "</th>";
-    for (var val of yAxis.values) {
-        if (!document.getElementById('showval_' + yAxis.id + '__' + val.key).checked) {
+    newContent += '</th>';
+    for (var y2val of (y2Axis == null ? [null] : y2Axis.values)) {
+        if (y2val != null && !canShowVal(y2Axis.id, y2val.key)) {
             continue;
         }
-        newContent += '<tr><td title="' + val.description.replaceAll('"', '&quot;') + '">' + val.title + '</td>';
-        var url = "";
-        for (var subAxis of rawData.axes) {
-            if (subAxis.id == x) {
-                url += '/{X}';
-            }
-            else if (subAxis.id == y) {
-                url += '/' + val.key;
-            }
-            else {
-                var valSelectedKey;
-                for (var subVal of subAxis.values) {
-                    if (window.getComputedStyle(document.getElementById('tab_' + subAxis.id + '__' + subVal.key)).display != 'none') {
-                        valSelectedKey = subVal.key;
-                    }
-                }
-                url += '/' + valSelectedKey;
-            }
-        }
-        for (var xVal of xAxis.values) {
-            if (!document.getElementById('showval_' + xAxis.id + '__' + xVal.key).checked) {
+        var y2first = true;
+        for (var val of yAxis.values) {
+            if (!canShowVal(yAxis.id, val.key)) {
                 continue;
             }
-            var actualUrl = url.replace('{X}', xVal.key).substring(1) + '.' + rawData.ext;
-            newContent += '<td><img class="table_img" id="autogen_img_' + escapeHtml(actualUrl).replace(' ', '%20') + '" onclick="doPopupFor(this)" src="' + actualUrl + '" /></td>';
-        }
-        newContent += '</tr>';
-        if (x == y) {
-            break;
+            newContent += '<tr><td class="axis_label_td" title="' + escapeHtml(val.description) + '">' + optDescribe(y2first, y2val) + val.title + '</td>';
+            y2first = false;
+            for (var x2val of (x2Axis == null ? [null] : x2Axis.values)) {
+                if (x2val != null && !canShowVal(x2Axis.id, x2val.key)) {
+                    continue;
+                }
+                newContent += getXAxisContent(x, y, xAxis, val, x2Axis, x2val, y2Axis, y2val);
+            }
+            newContent += '</tr>';
+            if (x == y) {
+                break;
+            }
         }
     }
     table.innerHTML = newContent;
     updateScaling();
 }
 
+function getCurrentSelectedAxis(axisPrefix) {
+    var id = document.querySelector('input[name="' + axisPrefix + '_axis_selector"]:checked').id;
+    var index = id.indexOf('_');
+    return id.substring(index + 1);
+}
+
 function updateScaling() {
     var percent;
-    var xAxis;
     if (document.getElementById('autoScaleImages').checked) {
-        var x = document.querySelector('input[name="x_axis_selector"]:checked').id.substring(2);
-        for (var axis of rawData.axes) {
-            if (axis.id == x) {
-                xAxis = axis;
-            }
+        var x = getCurrentSelectedAxis('x');
+        var xAxis = getAxisById(x);
+        var count = xAxis.values.length;
+        var x2 = getCurrentSelectedAxis('x2');
+        if (x2 != 'None') {
+            var x2Axis = getAxisById(x2);
+            count *= x2Axis.values.length;
         }
-        percent = (90 / xAxis.values.length) + 'vw';
+        percent = (90 / count) + 'vw';
     }
     else {
         percent = "";
@@ -125,7 +243,7 @@ function toggleDescriptions() {
 }
 
 function toggleShowVal(axis, val) {
-    var show = document.getElementById('showval_' + axis + '__' + val).checked;
+    var show = canShowVal(axis, val);
     var element = document.getElementById('clicktab_' + axis + '__' + val);
     if (show) {
         element.classList.remove('tab_hidden');
@@ -156,6 +274,34 @@ function enableRange(id) {
     return data;
 }
 
+function clickTabAfterActiveTab(tabs) {
+    var next = false;
+    for (var tab of tabs) {
+        if (tab.classList.contains('active')) {
+            next = true;
+        }
+        else if (tab.classList.contains('tab_hidden')) {
+            // Skip past
+        }
+        else if (next) {
+            tab.click();
+            return tab;
+        }
+    }
+    if (next) { // Click the first non-hidden
+        for (var tab of tabs) {
+            if (tab.classList.contains('tab_hidden')) {
+                // Skip past
+            }
+            else {
+                tab.click();
+                return tab;
+            }
+        }
+    }
+    return null;
+}
+
 async function startAutoScroll() {
     var rangeSet = [];
     for (var axis of rawData.axes) {
@@ -175,31 +321,7 @@ async function startAutoScroll() {
                 continue;
             }
             data.counter = 0;
-            var next = false;
-            for (var tab of data.tabs) {
-                if (tab.classList.contains('active')) {
-                    next = true;
-                }
-                else if (tab.classList.contains('tab_hidden')) {
-                    // Skip past
-                }
-                else if (next) {
-                    tab.click();
-                    next = false;
-                    break;
-                }
-            }
-            if (next) { // Click the first non-hidden
-                for (var tab of data.tabs) {
-                    if (tab.classList.contains('tab_hidden')) {
-                        // Skip past
-                    }
-                    else {
-                        tab.click();
-                        break;
-                    }
-                }
-            }
+            clickTabAfterActiveTab(data.tabs);
         }
     }
 }
@@ -236,12 +358,11 @@ function formatMetadata(valSet) {
     if (negative.length > 0) {
         negative = "\nNegative prompt: " + negative;
     }
-    // Keys we have but gen-param doesn't: VAE, sigma(churn/tmin/tmax/noise)
     var keyData = formatMet("Steps", valSet["steps"])
         + formatMet("Sampler", valSet["sampler"])
         + formatMet("CFG scale", valSet["cfgscale"])
         + formatMet("Seed", valSet["seed"])
-        // face restore
+        + formatMet("Face restoration", valSet["restorefaces"], "")
         + formatMet("Size", valSet["width"] + "x" + valSet["height"])
         // model hash
         + formatMet("Model", valSet["model"])
@@ -257,8 +378,19 @@ function formatMetadata(valSet) {
         + formatMet("Clip skip", valSet["clipskip"], "1")
         // ENSD
         ;
+        // Not part of normal gen-params
+    var extraData = formatMet("VAE", valSet["vae"])
+        + formatMet("Sigma Churn", valSet["sigmachurn"], "0")
+        + formatMet("Sigma T-Min", valSet["sigmatmin"], "0")
+        + formatMet("Sigma T-Max", valSet["sigmatmax"], "1")
+        + formatMet("Sigma Noise", valSet["sigmanoise"], "1")
+        + (valSet["restorefaces"] == "CodeFormer" ? formatMet("CodeFormer Weight", valSet["codeformerweight"]) : "")
+        ;
     keyData = keyData.substring(0, keyData.length - 2);
-    return valSet["prompt"] + negative + "\n" + keyData;
+    if (extraData.length > 2) {
+        extraData = extraData.substring(0, extraData.length - 2);
+    }
+    return valSet["prompt"] + negative + "\n" + keyData + "\n" + extraData;
 }
 
 function crunchMetadata(url) {
@@ -280,7 +412,7 @@ function crunchMetadata(url) {
             return { "error": "metadata parsing failed for part " + index + ": " + part };
         }
         for (var [key, value] of Object.entries(actualVal.params)) {
-            if (key == "promptreplace") {
+            if (key.replaceAll(' ', '') == "promptreplace") {
                 var replacers = value.split('=', 2);
                 var match = replacers[0].trim();
                 var replace = replacers[1].trim();
